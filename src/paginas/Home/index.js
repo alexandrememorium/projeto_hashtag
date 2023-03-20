4a61728af3bde034990880cb44073fc5ec631c32
Gabriel Gomes Fernandes <gabriel.gomes_fernandes@hotmail.com>
2023-03-20 16:20:05 -0300

    adicionando componente card

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -4,6 +4,7 @@ import Carrossel from '../../componentes/Carrossel';
 import Menu from '../../componentes/Menu/';
 import styles from './home.module.css';
 import Rodape from '../../componentes/Rodape'
+import Cartao from '../../componentes/CartaoDoUsuario';
 
 function Home() {
 
@@ -29,24 +30,7 @@ function Home() {
                     <Carrossel itens={itens}/>
                 </div>
 
-                <div className={styles.listaDeUsuarios}>
-                    {itens.map((_, index) => {
-                        return (
-                                <div className={styles.itemContent} key={index}>
-                                    <img src={`https://i.pravatar.cc/150?img=${index}`} alt="Imagem do Perfil" />
-                                    <div className={styles.usuarioInfo}>
-                                        <div>
-                                            <h3>UserName</h3>
-                                            <p>@twitterusername</p>
-                                        </div>
-                                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat...</p>
-                                        <a href="http://www.twitter.com">Ver mais no Twitter</a>
-                                        
-                                    </div>
-                                </div>
-                        )
-                    })}
-                </div>
+                <Cartao itens={itens}/>
 
             </div>
             <Rodape />
