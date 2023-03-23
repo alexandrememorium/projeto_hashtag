ab7343dbc1c170f04372da058ff7ebeb1c3b704f
igor <arqurb.igor@gmail.com>
2023-03-23 18:52:39 -0300

    ajuste app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Home from './paginas/Home/index.js';
-import Busca from './paginas/Busca/index.js';
 
 //paginas
 import Home from './paginas/Home/index.js';
