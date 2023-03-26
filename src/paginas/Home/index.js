50f7ec4e39fa106bd6560a4f56bd640d362a6c31
igor <arqurb.igor@gmail.com>
2023-03-26 18:28:34 -0300

    início inserção api buscas

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'swiper/swiper-bundle.min.css';
 import Carrossel from '../../componentes/Carrossel';
 import Menu from '../../componentes/Menu/';
@@ -9,6 +9,40 @@ import useFetch from '../../componentes/TwitterAPI/index.js'
 
 function Home() {
 
+    //busca
+    const [itemBusca, setItemBusca] = useState('');
+    /*console.log(itemBusca)*/
+
+    var myHeaders = new Headers();
+    myHeaders.append("content-type", "application/json");
+    myHeaders.append("Authorization", "Bearer keykXHtsEPprqdSBF");
+
+    var raw = JSON.stringify({
+        "records": [
+            {
+                "fields": {
+                    "Squad": "03-23",
+                    "Hashtag": {itemBusca},
+                    "Data": 123123
+                }
+            }
+        ]
+    })
+
+    function handleKeyPress(e) {
+        var key = e.key;
+        if (key === "Enter") {
+            console.log('você apertou enter')
+            fetch('https://api.airtable.com/v0/app6wQWfM6eJngkD4/Buscas?view=Grid%20view&api_key=keykXHtsEPprqdSBF&filterByFormula=Find(%2203-23%22%2C+Squad)', {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            })
+        }
+    }
+
+    //twitter
     const itens = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
     const texto = 'natureza'
@@ -25,7 +59,15 @@ function Home() {
             <div className={styles.header}>
                 <h1>Encontre hashtags<br></br> de maneira fácil.</h1>
                 <p>Digite o que deseja no campo de buscas e<br></br> confira os resultados do Twitter abaixo</p>
-                <input type={Text} name={'Busca'} placeholder={'Buscar...'}></input>
+                <input 
+                    type={Text} 
+                    name={'Busca'} 
+                    placeholder={'Buscar...'}
+                    onChange={itemBusca => setItemBusca(itemBusca.target.value)}
+                    value={itemBusca}
+                    onKeyPress={(e) => handleKeyPress(e)}
+                    requiredname={'Busca'} 
+                ></input>
             </div>
 
             <div className={styles.body}>
