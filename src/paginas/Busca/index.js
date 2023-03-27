556e5cf84217cb92f35b107864e4debb94d1ccd5
alexandrememorium <alexandre.souza@memoriumtech.com>
2023-03-27 07:58:06 -0300

    Atualização Git Pages

--- src/paginas/Busca/index.js
+++ src/paginas/Busca/index.js
@@ -1,13 +1,24 @@
 
 import React from 'react'
-import styles from './busca.module.css'
+import {Redirect} from 'react-router-dom';
+import styles from './busca.module.css';
 import Menu from '../../componentes/Menu';
 
-export default function index() {
+
+export default function Busca() {
+
+  let redirecionaBusca = false;
+
+    if(localStorage.getItem('logado') === 'true'){
+      redirecionaBusca = true;
+    }
+
   return (
-     
+    
     <div className={styles.fundoPag}>
-          
+
+      { redirecionaBusca === false ? <Redirect to="/" /> : '' } 
+
       <Menu headerHeightMobile={12.5} headerHeightDesktop={39.25}/>
       
       <div className={styles.Apptop}>
