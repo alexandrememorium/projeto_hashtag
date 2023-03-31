aa2b13fdcb32762926c988bc3e5c4d10d794cf5d
AlexHGama <alexhgama@gmail.com>
2023-03-31 00:03:43 -0300

    correção de código

--- src/paginas/Busca/index.js
+++ src/paginas/Busca/index.js
@@ -1,79 +1,73 @@
 import React from 'react'
+import { useState } from 'react';
 import styles from './busca.module.css'
 import Menu from '../../componentes/Menu';
 
 
 
 export default function Index() { 
-  const [] = useState('');
+  const [campos, setCampos] = useState([])
+  
+  React.useEffect(() => {
 
-    fetch("https://api.airtable.com/v0/app6wQWfM6eJngkD4/Buscas?view=Grid%20view&api_key=keykXHtsEPprqdSBF&filterByFormula=Find(%2203-23%22%2C+Squad)")
-     .then(response => response.json())
-     .then(res => res.records)
-     .catch(error => console.error(error))
-    
-    
-    const records = [];     
-    {     
-      <header className="container_mostra_busca">    
-        {records;slice(0, 10).map((record)  => {})                         
-            <div>
-              <div id='Hashtagname'>
-                <p id="Hashtagnamem" {record.id} />
-              </div>                     
-              <div className="Createdtime">
-                <p className="time">
-                  <b></b> {record.fields.Data}
-                </p>
-              </div>
-              <div className="Fields">
-                <p className="nome">
-                  <b></b> {record.fields.Hashtag}
-                </p>
-              </div>
-            </div>                     
+    fetch("https://api.airtable.com/v0/app6wQWfM6eJngkD4/Buscas?&pageSize=10&sort%5B0%5D%5Bfield%5D=Data&sort%5B0%5D%5Bdirection%5D=desc&view=Grid%20view&api_key=keykXHtsEPprqdSBF&filterByFormula=Find(%2203-23%22%2C+Squad)%22)"
+      .then(res => res.json())
+      .then(res => setCampos(res.records.map((record) => {
+        return {
+          hashtag: record.fields.Hashtag,
+          data: new Date(record.fields.Data)
         }
-      </header>
-    }
+      }))));
+
+  }, [])       
+   
   return (
      
     <div className={styles.fundoPag}>
-          
-      <Menu headerHeightMobile={12.5} headerHeightDesktop={39.25}/>
-      
+
+      <Menu headerHeightMobile={12.5} headerHeightDesktop={39.25} />
+
       <div className={styles.Apptop}>
-        
+
       </div >
       <div >
 
-      <div className={styles.buscas_realizadas}>
-        Buscas realizadas
-      </div>
-
-        <div className={styles.container_externo_hashtag}>
-
-        <div className={styles.container_hashtag}>
-
-          <div className={styles.hashtag}>
-            Hashtag
-          </div>
-
-          <div className={styles.data}>
-            Data
-          </div>
-
-          <div className={styles.hora}>
-            Hora
-          </div>
-
+        <div className={styles.buscas_realizadas}>
+          Buscas realizadas
         </div>
 
-      
-      <div className={styles.mostra_busca}>
-      </div>
-      </div>
+        <div className={styles.container_externo_hashtag}>
+          {campos.slice(0, 10).map((campo, index) => {
+            return (
+              <div className={styles.container_hashtag} key={index}>
+                <div className={styles.hashtag}>
+                  {campo.hashtag}
+                </div>
+
+                <div className={styles.data}>
+                {`${campo.data.toLocaleString('pt-BR',{
+                    month: '2-digit',
+                    day: '2-digit' 
+                  })}`}
+                </div>
+
+                <div className={styles.hora}>
+                  {`${campo.data.toLocaleString('pt-BR',{
+                    hour: '2-digit',
+                    minute: '2-digit'
+                  })}`}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className={styles.mostra_busca}>
+
+        </div>
       </div>
 
     </div>
+
+    
   );
 }
