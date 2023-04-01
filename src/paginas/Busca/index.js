a1de2ef720e9487b7e311f04928eeeb152a7fe28
alexandrememorium <alexandre.souza@memoriumtech.com>
2023-04-01 10:21:28 -0300

    Ajuste Fino Pag. Busca

--- src/paginas/Busca/index.js
+++ src/paginas/Busca/index.js
@@ -1,13 +1,22 @@
 
-import React, { useEffect, useState } from 'react'
-import styles from './busca.module.css'
+import React, { useEffect, useState } from 'react';
+import {Redirect} from 'react-router-dom';
+import styles from './busca.module.css';
 import Menu from '../../componentes/Menu';
 
 
 
-export default function Index() { 
+export default function Index() {
+  document.title = 'Projeto HashtagFinder - Busca';
+
+  let redirecionaBusca = false;
+
+  if (localStorage.getItem('logado') === 'true') {
+    redirecionaBusca = true;
+  }
+
   const [campos, setCampos] = useState([])
-  
+
   useEffect(() => {
 
     fetch("https://api.airtable.com/v0/app6wQWfM6eJngkD4/Buscas?&pageSize=10&sort%5B0%5D%5Bfield%5D=Data&sort%5B0%5D%5Bdirection%5D=desc&view=Grid%20view&api_key=keykXHtsEPprqdSBF&filterByFormula=Find(%2203-23%22%2C+Squad)")
@@ -19,60 +28,60 @@ export default function Index() {
         }
       })));
 
-  }, [])       
-   
-  return (
-     
-    <div className={styles.fundoPag}>
+  }, [])
 
+  return (
+
+    <div className={styles.fundoPag}>
+      { redirecionaBusca === false ? <Redirect to="/" /> : '' } 
       <Menu headerHeightMobile={12.5} headerHeightDesktop={39.25} />
 
       <div className={styles.buscas_realizadas}>
         Buscas realizadas
       </div>
-    <div className={styles.caixa}>
-      <div className={styles.cabecalho}>        
-        <p className={styles.hashtag_cabecalho}>Hashtag</p>
-        <p className={styles.data_cabecalho}>Data</p>
-        <p className={styles.hora_cabecalho}>Hora</p>       
-      </div>
+      <div className={styles.caixa}>
+        <div className={styles.cabecalho}>
+          <p className={styles.hashtag_cabecalho}>Hashtag</p>
+          <p className={styles.data_cabecalho}>Data</p>
+          <p className={styles.hora_cabecalho}>Hora</p>
+        </div>
 
         <div style={{
-           padding:'0px 20px',
-           textTransform:'lowercase'
+          padding: '0px 20px',
+          textTransform: 'lowercase'
         }}>
-        <section className={styles.teste}>  
-          {campos.slice(0, 10).map((campo, index) => {
-            return (
-              
-              
-              <div className={styles.container_hashtag} key={index}>
-               
-               
-                
+          <section className={styles.teste}>
+            {campos.slice(0, 10).map((campo, index) => {
+              return (
 
-                <div className={styles.hashtag}>
-                  #{campo.hashtag}
+
+                <div className={styles.container_hashtag} key={index}>
+
+
+
+
+                  <div className={styles.hashtag}>
+                    #{campo.hashtag}
+                  </div>
+
+                  <div className={styles.data}>
+                    {`${campo.data.toLocaleString('pt-BR', {
+                      month: '2-digit',
+                      day: '2-digit'
+                    })}`}
+                  </div>
+
+                  <div className={styles.hora}>
+                    {`${campo.data.toLocaleString('pt-BR', {
+                      hour: '2-digit',
+                      minute: '2-digit'
+                    })}`}
+                  </div>
+
                 </div>
 
-                <div className={styles.data}>
-                {`${campo.data.toLocaleString('pt-BR',{
-                    month: '2-digit',
-                    day: '2-digit' 
-                  })}`}
-                </div>
-
-                <div className={styles.hora}>
-                  {`${campo.data.toLocaleString('pt-BR',{
-                    hour: '2-digit',
-                    minute: '2-digit'
-                  })}`}
-                </div>
-                
-              </div>
-              
-            )
-          })}
+              )
+            })}
           </section>
         </div>
         <div className={styles.mostra_busca}>
@@ -82,6 +91,6 @@ export default function Index() {
 
     </div>
 
-    
+
   );
 }
