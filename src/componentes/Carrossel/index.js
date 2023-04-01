43e4e5c35526cab8448acfdb92a6db4d3f586f1c
alexandrememorium <alexandre.souza@memoriumtech.com>
2023-04-01 13:33:41 -0300

    Ajuste Carrocel

--- src/componentes/Carrossel/index.js
+++ src/componentes/Carrossel/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Navigation, Pagination } from 'swiper'
 import { SwiperSlide, Swiper } from 'swiper/react'
 import './swiper.css'
